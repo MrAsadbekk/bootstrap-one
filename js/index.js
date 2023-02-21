7152ed2558983fc19baa0a43cc563ad9ac7394f9
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-02-21 21:55:08 +0300

    one-bootstrap

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var a = 2023;
+var b = 2003;
+var name = " Asadbek";
+var firstname = " Qoryog'diyev";
+var info =
+  "salom mening ismim" + name + firstname + " men " + (a - b) + " yoshdaman";
+console.log(info);
